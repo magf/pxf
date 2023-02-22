11c35dfd39663d956bda31024f858498d4affa4f
Alexey Gordeev <goa@arenadata.io>
2023-02-22 12:45:33 +0500

    PXF Docker environment for tests running (#33)
    * PXF Docker environment for tests running
    
    The goal of this patch is to begin automation of PXF testing. It contains
    scripts and images for Docker environment, like it's done for other ADB
    components and extensions. Existing upstream concourse scripts were reused where
    it was possible. For now, we don't focus on hadoop integration testing, but do
    unit testing, fdw and external-table regression testing.
    
    * ADBDEV-3356: Fix ClassCastException issue in DemoTextResolver
    
    If the output format of the external table is 'TEXT' we return DataInputStream instead of byte array
    
    ---------
    
    Co-authored-by: Roman Zolotov <rzolotov@arenadata.io>

--- server/pxf-api/src/main/java/org/greenplum/pxf/api/examples/DemoTextResolver.java
+++ server/pxf-api/src/main/java/org/greenplum/pxf/api/examples/DemoTextResolver.java
@@ -23,6 +23,8 @@ import org.greenplum.pxf.api.OneField;
 import org.greenplum.pxf.api.OneRow;
 import org.greenplum.pxf.api.io.DataType;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -62,8 +64,16 @@ public class DemoTextResolver extends DemoResolver {
             throw new Exception("Unexpected record format, expected 1 field, found " +
                     (record == null ? 0 : record.size()));
         }
-        byte[] value = (byte[]) record.get(0).val;
+        int readCount;
+        byte[] data = new byte[1024];
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        DataInputStream dis = (DataInputStream) record.get(0).val;
+        while ((readCount = dis.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, readCount);
+        }
+        buffer.flush();
+        byte[] bytes= buffer.toByteArray();
         // empty array means the end of input stream, return null to stop iterations
-        return value.length == 0 ? null : new OneRow(value);
+        return bytes.length == 0 ? null : new OneRow(bytes);
     }
 }
