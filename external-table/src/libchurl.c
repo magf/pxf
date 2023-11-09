95bd59c14fa5a3dad1cd2c1b5c42b901ba19531b
Viktor Kurilko <v.kurilko@arenadata.io>
2023-11-09 16:38:10 +0700

    Revert "ADBDEV-2976: Handle multi_perform curl errors immediately (#24)"
    This reverts commit 93f41a7477d668dda01cb9baca0a39a53f6958a6.

--- external-table/src/libchurl.c
+++ external-table/src/libchurl.c
@@ -526,6 +526,7 @@ churl_read_check_connectivity(CHURL_HANDLE handle)
 	Assert(!context->upload);
 
 	fill_internal_buffer(context, 1);
+	check_response(context);
 }
 
 /*
@@ -703,8 +704,6 @@ multi_perform(churl_context *context)
 	if (curl_error != CURLM_OK)
 		elog(ERROR, "internal error: curl_multi_perform failed (%d - %s)",
 			 curl_error, curl_easy_strerror(curl_error));
-
-	check_response(context);
 }
 
 static bool
@@ -731,6 +730,8 @@ flush_internal_buffer(churl_context *context)
 
 		multi_perform(context);
 	}
+
+	check_response(context);
 
 	if ((context->curl_still_running == 0) &&
 		((context_buffer->top - context_buffer->bot) > 0))
@@ -786,6 +787,8 @@ finish_upload(churl_context *context)
 	 */
 	while (context->curl_still_running != 0)
 		multi_perform(context);
+
+	check_response(context);
 }
 
 static void
--- fdw/libchurl.c
+++ fdw/libchurl.c
@@ -533,6 +533,7 @@ churl_read_check_connectivity(CHURL_HANDLE handle)
 	Assert(!context->upload);
 
 	fill_internal_buffer(context, 1);
+	check_response(context);
 }
 
 /*
@@ -710,8 +711,6 @@ multi_perform(churl_context *context)
 	if (curl_error != CURLM_OK)
 		elog(ERROR, "internal error: curl_multi_perform failed (%d - %s)",
 			 curl_error, curl_easy_strerror(curl_error));
-
-	check_response(context);
 }
 
 static bool
@@ -744,6 +743,8 @@ flush_internal_buffer(churl_context *context)
 	if ((context->curl_still_running == 0) &&
 		((context_buffer->top - context_buffer->bot) > 0))
 		elog(ERROR, "failed sending to remote component %s", get_dest_address(context->curl_handle));
+
+	check_response(context);
 
 	context_buffer->top = 0;
 	context_buffer->bot = 0;
@@ -796,6 +797,8 @@ finish_upload(churl_context *context)
 	 */
 	while (context->curl_still_running != 0)
 		multi_perform(context);
+
+	check_response(context);
 }
 
 static void
