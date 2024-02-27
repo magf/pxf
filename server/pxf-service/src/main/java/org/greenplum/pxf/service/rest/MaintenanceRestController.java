20f23de6f2bb234a81f2aad33f557e33183ee457
Roman Zolotov <rzolotov@arenadata.io>
2024-02-27 20:20:22 +0200

    ADBDEV-4985: Add @RequiredArgsConstructor annotation

--- server/pxf-service/src/main/java/org/greenplum/pxf/service/rest/MaintenanceRestController.java
+++ server/pxf-service/src/main/java/org/greenplum/pxf/service/rest/MaintenanceRestController.java
@@ -1,5 +1,6 @@
 package org.greenplum.pxf.service.rest;
 
+import lombok.RequiredArgsConstructor;
 import org.greenplum.pxf.service.profile.ProfileReloadService;
 import org.greenplum.pxf.service.rest.dto.ProfileReloadRequestDto;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,13 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/pxf")
 public class MaintenanceRestController {
     private final ProfileReloadService profileReloadService;
-
-    public MaintenanceRestController(final ProfileReloadService profileReloadService) {
-        this.profileReloadService = profileReloadService;
-    }
 
     @PostMapping(value = "/reload")
     public void reload(@RequestBody ProfileReloadRequestDto reloadRequestDto) {
