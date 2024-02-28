2fc31559c331a9d1062531fdfd01b25208c847a5
Roman Zolotov <rzolotov@arenadata.io>
2024-02-28 11:59:18 +0200

    ADBDEV-4986: Implemented service to reload profile

--- server/pxf-service/src/main/java/org/greenplum/pxf/service/profile/ProfileReloadServiceImpl.java
+++ server/pxf-service/src/main/java/org/greenplum/pxf/service/profile/ProfileReloadServiceImpl.java
@@ -1,12 +1,91 @@
 package org.greenplum.pxf.service.profile;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.greenplum.pxf.api.error.PxfRuntimeException;
+import org.greenplum.pxf.api.model.Plugin;
 import org.greenplum.pxf.service.rest.dto.ProfileReloadRequestDto;
-import org.springframework.stereotype.Service;
+import org.greenplum.pxf.service.utilities.BasePluginFactory;
+import org.springframework.stereotype.Component;
 
-@Service
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class ProfileReloadServiceImpl implements ProfileReloadService {
+    private final static String ACCESSOR_KEY = "ACCESSOR";
+    private final ProfilesConf profileConf;
+    private final BasePluginFactory basePluginFactory;
+
     @Override
     public void reloadProfile(ProfileReloadRequestDto reloadRequestDto) {
-        // Will be implemented as a part of the ADBDEV-4986 task
+        String profile = reloadRequestDto.getProfile();
+        String server = reloadRequestDto.getServer();
+        log.info("Received a request to reload a profile with the parameters: profile={}, server={}", profile, server);
+
+        if (StringUtils.isBlank(profile)) {
+            if (StringUtils.isBlank(server)) {
+                // TODO: Terminate all active queries before reload (ADBDEV-4987)
+                profileConf.getProfilesMap().keySet()
+                        .forEach(this::reloadAll);
+            } else {
+                throw new IllegalArgumentException(String.format("The provided parameters (profile=%s, server=%s) " +
+                        "are not correct. Please add profile", profile, server));
+            }
+        } else if (StringUtils.isBlank(server)) {
+            // TODO: Terminate all active queries before reload (ADBDEV-4987)
+            reloadAll(profile);
+        } else {
+            // TODO: Terminate all active queries before reload (ADBDEV-4987)
+            reload(profile, server);
+        }
+    }
+
+    private void reloadAll(String profile) {
+        Plugin accessor = getAccessor(profile);
+        if (Objects.nonNull(accessor)) {
+            log.info("Reload profile '{}' for all servers with accessor {}", profile, accessor);
+            accessor.reloadAll();
+        } else {
+            log.info("Skipping reloading profile '{}' for all servers", profile);
+        }
+    }
+
+    private void reload(String profile, String server) {
+        Plugin accessor = getAccessor(profile);
+        if (Objects.nonNull(accessor)) {
+            log.info("Reload profile '{}' for server '{}' with accessor {}", profile, server, accessor);
+            accessor.reload(server);
+        } else {
+            log.info("Skipping reloading profile '{}' for server '{}'", profile, server);
+        }
+    }
+
+    private Plugin getAccessor(String profile) {
+        try {
+            Map<String, String> pluginMap = profileConf.getPlugins(profile);
+            return Optional.ofNullable(pluginMap)
+                    .map(plugins -> getAccessorInstance(plugins.get(ACCESSOR_KEY)))
+                    .orElse(null);
+        } catch (Exception e) {
+            String message = String.format("Failed to get plugin. %s", e.getMessage());
+            log.error(message);
+            throw new PxfRuntimeException(message, e);
+        }
+    }
+
+    private Plugin getAccessorInstance(String accessorClassName) {
+        try {
+            Plugin instance = basePluginFactory.getPluginInstance(accessorClassName);
+            log.debug("Initialize instance {} of the accessor class {}", instance, accessorClassName);
+            return instance;
+        } catch (Exception e) {
+            log.error("Failed to initialize instance of the accessor class {}. {}", accessorClassName, e.getMessage());
+            return null;
+        }
     }
 }
