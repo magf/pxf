3a943b6df68d85f84428b541508f91bc61103a7e
Roman Zolotov <rzolotov@arenadata.io>
2024-03-04 17:33:49 +0200

    ADBDEV-4986: Refactoring service to reload profile
    -Add ProfileReloadConfiguration to initialize map with profiles which support reloading;

--- server/pxf-service/src/main/java/org/greenplum/pxf/service/profile/ProfileReloadServiceImpl.java
+++ server/pxf-service/src/main/java/org/greenplum/pxf/service/profile/ProfileReloadServiceImpl.java
@@ -1,89 +1,70 @@
 package org.greenplum.pxf.service.profile;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.greenplum.pxf.api.error.PxfRuntimeException;
-import org.greenplum.pxf.api.model.Plugin;
+import org.greenplum.pxf.api.model.Reloader;
 import org.greenplum.pxf.service.rest.dto.ProfileReloadRequestDto;
-import org.greenplum.pxf.service.utilities.BasePluginFactory;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-import java.util.Optional;
+import java.util.Objects;
+import java.util.function.Predicate;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class ProfileReloadServiceImpl implements ProfileReloadService {
-    private final static String ACCESSOR_KEY = "ACCESSOR";
-    private final ProfilesConf profileConf;
-    private final BasePluginFactory basePluginFactory;
+    private final Map<String, Reloader> profileReloaderMap;
+
+    public ProfileReloadServiceImpl(@Lazy Map<String, Reloader> profileReloaderMap) {
+        this.profileReloaderMap = profileReloaderMap;
+    }
 
     @Override
     public void reloadProfile(ProfileReloadRequestDto reloadRequestDto) {
         String profile = reloadRequestDto.getProfile();
         String server = reloadRequestDto.getServer();
         log.info("Received a request to reload a profile with the parameters: profile={}, server={}", profile, server);
-
-        if (StringUtils.isBlank(profile)) {
-            if (StringUtils.isBlank(server)) {
-                // TODO: Terminate all active queries before reload (ADBDEV-4987)
-                profileConf.getProfilesMap().keySet()
-                        .forEach(this::reloadAll);
-            } else {
-                throw new IllegalArgumentException(String.format("The provided parameters (profile=%s, server=%s) " +
-                        "are not correct. Please add profile", profile, server));
-            }
-        } else if (StringUtils.isBlank(server)) {
-            // TODO: Terminate all active queries before reload (ADBDEV-4987)
+        if (StringUtils.isBlank(server)) {
             reloadAll(profile);
-        } else {
-            // TODO: Terminate all active queries before reload (ADBDEV-4987)
+        } else if (StringUtils.isNotBlank(profile)) {
             reload(profile, server);
+        } else {
+            throw new IllegalArgumentException(String.format("The provided parameters (profile=%s, server=%s) " +
+                    "are not correct. Please add profile", profile, server));
         }
     }
 
     private void reloadAll(String profile) {
-        Optional<Plugin> accessor = getAccessor(profile);
-        if (accessor.isPresent()) {
-            log.info("Reload profile '{}' for all servers with accessor {}", profile, accessor.get());
-            accessor.get().reloadAll();
-        } else {
-            log.info("Skipping reloading profile '{}' for all servers", profile);
-        }
+        Predicate<String> profilePredicate = key -> (StringUtils.isBlank(profile) || key.equals(profile));
+        profileReloaderMap.forEach((profileName, reloader) -> {
+            if (profilePredicate.test(profileName)) {
+                // TODO: Terminate all active queries before reload (ADBDEV-4987)
+                reloader.reloadAll();
+                log.info("Reload profile '{}' for all servers with reloader {}", profileName, reloader);
+            } else {
+                String message = String.format(
+                        "Profile '%s' doesn't support reloading methods. Skipping reloading for all servers. " +
+                                "Profiles with supporting reloading methods: %s", profile, profileReloaderMap.keySet());
+                log.error(message);
+                throw new PxfRuntimeException(message);
+            }
+        });
     }
 
     private void reload(String profile, String server) {
-        Optional<Plugin> accessor = getAccessor(profile);
-        if (accessor.isPresent()) {
-            log.info("Reload profile '{}' for server '{}' with accessor {}", profile, server, accessor.get());
-            accessor.get().reload(server);
+        Reloader reloader = profileReloaderMap.get(profile);
+        if (Objects.nonNull(reloader)) {
+            // TODO: Terminate all active queries before reload (ADBDEV-4987)
+            reloader.reload(server);
+            log.info("Reload profile '{}' for server '{}' with reloader {}", profile, server, reloader);
         } else {
-            log.info("Skipping reloading profile '{}' for server '{}'", profile, server);
-        }
-    }
-
-    private Optional<Plugin> getAccessor(String profile) {
-        try {
-            Map<String, String> pluginMap = profileConf.getPlugins(profile);
-            return Optional.ofNullable(pluginMap)
-                    .map(plugins -> getAccessorInstance(plugins.get(ACCESSOR_KEY)));
-        } catch (Exception e) {
-            String message = String.format("Failed to get plugin of the profile '%s'. %s", profile, e.getMessage());
+            String message = String.format(
+                    "Profile '%s' doesn't support reloading methods. Skipping reloading for server '%s'. " +
+                            "Profiles with supporting reloading methods: %s", profile, server, profileReloaderMap.keySet());
             log.error(message);
-            throw new PxfRuntimeException(message, e);
-        }
-    }
-
-    private Plugin getAccessorInstance(String accessorClassName) {
-        try {
-            Plugin instance = basePluginFactory.getPluginInstance(accessorClassName);
-            log.debug("Initialize instance {} of the accessor class {}", instance, accessorClassName);
-            return instance;
-        } catch (Exception e) {
-            log.error("Failed to initialize instance of the accessor class {}. {}", accessorClassName, e.getMessage());
-            return null;
+            throw new PxfRuntimeException(message);
         }
     }
 }
