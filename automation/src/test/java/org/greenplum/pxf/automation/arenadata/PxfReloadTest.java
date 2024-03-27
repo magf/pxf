ba3d46cf8dcf39cf3f3c59200a0c4775d02e7d19
r.timeryarov <r.timeryarov@arenadata.io>
2024-03-27 19:35:50 +0300

    ADBDEV-4983: [IFT] PXF reload

---
+++ automation/src/test/java/org/greenplum/pxf/automation/arenadata/PxfReloadTest.java
@@ -0,0 +1,126 @@
+package org.greenplum.pxf.automation.arenadata;
+
+import org.greenplum.pxf.automation.components.cluster.MultiNodeCluster;
+import org.greenplum.pxf.automation.components.cluster.PhdCluster;
+import org.greenplum.pxf.automation.components.cluster.installer.nodes.Node;
+import org.greenplum.pxf.automation.components.cluster.installer.nodes.SegmentNode;
+import org.greenplum.pxf.automation.features.BaseFeature;
+import org.greenplum.pxf.automation.structures.tables.basic.Table;
+import org.greenplum.pxf.automation.structures.tables.pxf.ExternalTable;
+import org.greenplum.pxf.automation.structures.tables.utils.TableFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.greenplum.pxf.automation.PxfTestConstant.PXF_JDBC_SITE_CONF_FILE_PATH_TEMPLATE;
+
+public class PxfReloadTest extends BaseFeature {
+    private Node pxfNode;
+    private String pxfLogFile;
+    private static final String PXF_LOG_RELATIVE_PATH = "logs/pxf-service.log";
+    private static final String PXF_RELOAD_SERVER_PROFILE = "reload";
+    private static final String PXF_RELOAD_SECOND_SERVER_PROFILE = "reload-second";
+    private static final String PXF_JDBC_SITE_CONF_TEMPLATE_RELATIVE_PATH = "templates/pxf-reload/jdbc-site.xml";
+    private static final String[] TABLE_FIELDS = new String[]{
+            "id  int",
+            "name text"};
+
+    @Override
+    protected void beforeClass() throws Exception {
+        String pxfHome = cluster.getPxfHome();
+        String pxfJdbcSiteConfTemplate = pxfHome + "/" + PXF_JDBC_SITE_CONF_TEMPLATE_RELATIVE_PATH;
+
+        String pxfJdbcSiteConfPath = String.format(PXF_JDBC_SITE_CONF_FILE_PATH_TEMPLATE, pxfHome, PXF_RELOAD_SERVER_PROFILE);
+        cluster.copyFileToNodes(pxfJdbcSiteConfTemplate, pxfJdbcSiteConfPath, true, false);
+
+        String pxfSecondJdbcSiteConfPath = String.format(PXF_JDBC_SITE_CONF_FILE_PATH_TEMPLATE, pxfHome, PXF_RELOAD_SECOND_SERVER_PROFILE);
+        cluster.copyFileToNodes(pxfJdbcSiteConfTemplate, pxfSecondJdbcSiteConfPath, true, false);
+
+        if (cluster instanceof MultiNodeCluster) {
+            pxfNode = ((MultiNodeCluster) cluster).getNode(SegmentNode.class, PhdCluster.EnumClusterServices.pxf).get(0);
+        }
+        pxfLogFile = pxfHome + "/" + PXF_LOG_RELATIVE_PATH;
+    }
+
+    @Test(groups = {"arenadata"})
+    public void reloadAll() throws Exception {
+        String extTable1 = prepareTables("table", PXF_RELOAD_SERVER_PROFILE);
+        String extTable2 = prepareTables("table2", PXF_RELOAD_SECOND_SERVER_PROFILE);
+
+        gpdb.runQuery("select * from " + extTable1, true, true);
+        gpdb.runQuery("select * from " + extTable2, true, true);
+
+        int sessionCountBeforeReload = getGpdbSessionCount();
+        cluster.runCommandOnNodes(Collections.singletonList(pxfNode), "> " + pxfLogFile);
+        cluster.runCommand("pxf cluster reload -a", 0);
+        int sessionCountAfterReload = getGpdbSessionCount();
+
+        Assert.assertEquals(sessionCountBeforeReload - sessionCountAfterReload, 2, "Two sessions should be closed");
+        cluster.runCommand("cat " + pxfLogFile);
+        List<String> result = Arrays.asList(cluster.getLastCmdResult().split("\r\n"));
+        Assert.assertTrue(result.stream().anyMatch(logLine -> logLine.contains("Received a request to reload a profile with the parameters: profile=, server=")),
+                "Pxf reload not found in log");
+        Assert.assertEquals(result.stream().filter(logLine -> logLine.contains("Shutdown completed.")).count(), 2, "Two HikariPool should be reloaded");
+    }
+
+    @Test(groups = {"arenadata"})
+    public void reloadOneServerProfile() throws Exception {
+        String extTable1 = prepareTables("table", PXF_RELOAD_SERVER_PROFILE);
+        String extTable2 = prepareTables("table2", PXF_RELOAD_SECOND_SERVER_PROFILE);
+
+        gpdb.runQuery("select * from " + extTable1, true, true);
+        gpdb.runQuery("select * from " + extTable2, true, true);
+
+        int sessionCountBeforeReload = getGpdbSessionCount();
+        cluster.runCommandOnNodes(Collections.singletonList(pxfNode), "> " + pxfLogFile);
+        cluster.runCommand("pxf cluster reload -a -p jdbc -s " + PXF_RELOAD_SERVER_PROFILE, 0);
+        int sessionCountAfterReload = getGpdbSessionCount();
+        Assert.assertEquals(sessionCountBeforeReload - sessionCountAfterReload, 1, "One sessions should be closed");
+        cluster.runCommand("cat " + pxfLogFile);
+        List<String> result = Arrays.asList(cluster.getLastCmdResult().split("\r\n"));
+        Assert.assertTrue(result.stream().anyMatch(logLine -> logLine.contains("Received a request to reload a profile with the parameters: profile=jdbc, server=reload")),
+                "Pxf reload not found in log");
+        Assert.assertEquals(result.stream().filter(logLine -> logLine.contains("Shutdown completed.")).count(), 1, "One HikariPool should be reloaded");
+    }
+
+    private int getGpdbSessionCount() throws Exception {
+        Table gpStatActivityResult = TableFactory.getPxfJdbcReadableTable("gpStatActivityResult",
+                null, null, null);
+        gpdb.queryResults(gpStatActivityResult, "select * from pg_stat_activity where usename = 'gpadmin';");
+        return gpStatActivityResult.getData().size();
+    }
+
+    private String prepareTables(String tableName, String serverProfile) throws Exception {
+        String extTableName = "read_ext_" + tableName;
+        prepareSourceTable("gpdb_source_" + tableName);
+        createExternalTable(extTableName, "public.gpdb_source_" + tableName, serverProfile);
+        return extTableName;
+    }
+
+    private void createExternalTable(String tableName, String dataSourcePath, String serverProfile) throws Exception {
+        ExternalTable pxfJdbcNamedQuery = TableFactory.getPxfJdbcReadableTable(
+                tableName,
+                TABLE_FIELDS,
+                dataSourcePath,
+                serverProfile);
+        pxfJdbcNamedQuery.setHost(pxfHost);
+        pxfJdbcNamedQuery.setPort(pxfPort);
+        gpdb.createTableAndVerify(pxfJdbcNamedQuery);
+    }
+
+    private void prepareSourceTable(String tableName) throws Exception {
+        Table gpdbSourceTable = new Table(tableName, TABLE_FIELDS);
+        gpdbSourceTable.setDistributionFields(new String[]{"name"});
+        gpdb.createTableAndVerify(gpdbSourceTable);
+        String[][] rows = new String[][]{
+                {"1", "text1"},
+                {"2", "text2"},
+                {"3", "text3"}};
+        Table dataTable = new Table("dataTable", TABLE_FIELDS);
+        dataTable.addRows(rows);
+        gpdb.insertData(dataTable, gpdbSourceTable);
+    }
+}
