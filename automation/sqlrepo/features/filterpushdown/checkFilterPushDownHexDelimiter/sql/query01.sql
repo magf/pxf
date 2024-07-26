58c933c01d3d91c71fd6b604e96971d650112d75
Alexander Kondakov <a.kondakov@arenadata.io>
2024-07-26 15:10:09 +0300

    ADBDEV-5772 [C] Extend list of types to predicate pushdown (#104)
    Extend the list of types for predicate pushdown
    
    The recent changes to the pxffilters.c file in the external-table codebase and
    in pxf_filter.c in the fdw codebase introduce several enhancements and
    adjustments aimed at improving filter handling, particularly extending of
    supported data types range.
    Here is a summary of the key updates:
    
    Array comparison support in the filters:
    The operator map for pxf_supported_opr_op_expr now includes generic array
    comparison operators like ARRAY_EQ_OP, enhancing the filter capabilities for
    array types. Only = , <>, IN, IS NULL operators are supported. If the array's
    elements do not support any scalar operator, the error is thrown in order
    to avoid extra work of pxf (otherwise GPDB will reject the result and throw an
    error after the data has been processed and returned by the pxf).
    A new oid list pxf_supported_array_types[] and a new function
    supported_array_type(Oid type) are added to check if a given array type is
    supported for filtering. It is used to process array as list constants
    inside the opexpr_to_pxffilter() (OpExprToPxfFilter()) function.
    pxf_serialize_filter_list() (PxfSerializeFilterList()) is updated to handle
    scenarios where a filter operand is an attribute and the other is a list
    constant.
    
    List Constant Handling:
    The function list_const_to_str() (ListConstToStr()) now includes an additional
    bool parameter with_nulls, allowing it to handle NULL values in array constants
    appropriately. For the case of IN operators (scalar_array_op_expr_to_pxffilter()
    ) the NULL value inside the enumeration is meaningless (the comparison with
    NULLS can't be performed) and the list_const_to_str() is called with false
    with_nulls argument. For the case of array comparison (like a =
    ARRAY[TRUE, NULL]) the list_const_to_str() is called with true parameter
    with_nulls.
    
    BOOLARRAY type introduction:
    New OID BOOLARRAYOID is defined as a new macro and is covered in #ifndef
    because GPDB 6 does not have the definition for this type.
    The patch makes updates to pxf_supported_opr_scalar_array_op_expr with new entry
    BooleanEqualOperator to handle the IN operator for BOOL type. The
    list_const_to_str() (ListConstToStr()) also uses predefined string constants
    to encode the bool values in the final filter representation.
    
    Other types are handled by extending pxf_supported_opr_op_expr[],
    pxf_supported_types[], pxf_supported_array_types[],
    pxf_supported_opr_scalar_array_op_expr[] lists and corresponding switch
    operators inside the scalar_const_to_str() and list_const_to_str() functions.
    
    The following new data types have been added or extended for array types:
    
    BYTEA, BYTEAARRAY;
    FLOAT4, FLOAT8, FLOAT4ARRAY, FLOAT8ARRAY;
    BPCHARARRAY;
    VARCHARARRAYOID (here the modification of scalar_array_op_expr_to_pxffilter()
    was needed due to RelabelType occurence in queries like SELECT * from
    test_varchar where t in ('aaa'::varchar(10), 'bbb'::varchar(10));
    DATEARRAY;
    TIME, TIMEARRAY;
    TIMESTAMPARRAY;
    TIMESTAMPTZ, TIMESTAMPTZARRAY;
    INTERVAL, INTERVALARRAYOID;
    NUMERICARRAY;
    UUID, UUIDARRAY;
    JSONB, JSONBARRAY;
    JSON, JSONARRAY (scalar operators and IN are not supported);
    
    In order to launch the regression tests the UserDataVerifyAccessor.java
    has been extended to generate more fields of newly supported types.
    Automation tests checkFilterPushdown were modified and extendted
    to cover most of the newly added types.

--- automation/sqlrepo/features/filterpushdown/checkFilterPushDownHexDelimiter/sql/query01.sql
+++ automation/sqlrepo/features/filterpushdown/checkFilterPushDownHexDelimiter/sql/query01.sql
@@ -3,8 +3,8 @@ SET gp_external_enable_filter_pushdown = true;
 
 SET optimizer = off;
 
-SELECT * FROM test_filter WHERE t0 = 'C' AND a1 = 2 ORDER BY t0, a1;
+SELECT t0, a1, b2, c3, d4, e5, filterValue FROM test_filter WHERE t0 = 'C' AND a1 = 2 ORDER BY t0, a1;
 
 SET optimizer = on;
 
-SELECT * FROM test_filter WHERE t0 = 'C' AND a1 = 2 ORDER BY t0, a1;
+SELECT t0, a1, b2, c3, d4, e5, filterValue FROM test_filter WHERE t0 = 'C' AND a1 = 2 ORDER BY t0, a1;
--- server/pxf-diagnostic/src/main/java/org/greenplum/pxf/diagnostic/UserDataVerifyAccessor.java
+++ server/pxf-diagnostic/src/main/java/org/greenplum/pxf/diagnostic/UserDataVerifyAccessor.java
@@ -46,9 +46,42 @@ public class UserDataVerifyAccessor extends BasePlugin implements Accessor {
                 .add(counter == 1 ? NULL : String.valueOf(counter))               // integer
                 .add(counter == 2 ? NULL : String.valueOf(counter % 2 == 0))   // boolean
                 .add(counter == 3 ? NULL : String.format("%1$d.%1$d1", counter))  // numeric, decimal, real, double precision
-                .add(counter == 4 ? NULL : "" + textColumn + textColumn)          // bpchar
+                .add(counter == 4 ? NULL : "" + textColumn + textColumn)          // char
                 .add(counter == 5 ? NULL : "" + textColumn + textColumn)          // varchar
-                .add(filter)
+                .add(counter == 0 ? NULL : "" + textColumn + textColumn)          // bpchar
+                .add(counter == 1 ? NULL : String.valueOf(counter))           // int2
+                .add(counter == 2 ? NULL : String.valueOf(counter))           // int8
+                .add(counter == 3 ? NULL : String.format("%1$d.%1$d1", counter)) // real
+                .add(counter == 4 ? NULL : String.format("%1$d.%1$d1", counter)) // float8
+                .add(counter == 5 ? NULL : "\\x5a677265656e706c756d5a")       // bytea
+                .add(counter == 0 ? NULL : "2023-01-1" + counter % 10)        // date
+                .add(counter == 1 ? NULL : "12:34:5" + counter % 10)          // time
+                .add(counter == 2 ? NULL : "2023-01-01 12:34:5" + counter % 10)   // timestamp
+                .add(counter == 3 ? NULL : "2023-01-01 12:34:56 +00:0" + counter % 7)  // TIMESTAMP_WITH_TIME_ZONE
+                .add(counter == 4 ? NULL : "1 hour 3" + counter + " minutes")   // INTERVAL
+                .add(counter == 5 ? NULL : "93d8f9c0-c314-447b-8690-60c40facb8a"  + counter % 10)        // UUID
+                .add(counter == 0 ? NULL : "\"{\"\"a\"\":" + counter + "}\"")   // JSON
+                .add(counter == 1 ? NULL : "\"{\"\"a\"\":" + counter + "}\"")   // JSONB
+                .add(counter == 2 ? NULL : "\"{" + counter + "," + (counter + 1) + "}\"")       // INT2ARRAY
+                .add(counter == 3 ? NULL : "\"{" + (counter + 2) + "," + (counter + 3) + "}\"") // INT4ARRAY
+                .add(counter == 4 ? NULL : "\"{" + (counter + 4) + "," + (counter + 5) + "}\"") // INT8ARRAY
+                .add(counter == 5 ? NULL : "\"{" + String.valueOf(counter % 2 == 0) + "," + String.valueOf(counter % 2 == 1) + "," + "null" + "}\"")   // BOOLARRAY
+                .add(counter == 0 ? NULL : "\"{" + textColumn + "," + textColumn + "}\"")                                         // TEXTARRAY
+                .add(counter == 1 ? NULL : "\"{" + (counter + 1.1f) + "," + (counter + 2.1f) + "}\"")                             // FLOAT4ARRAY
+                .add(counter == 2 ? NULL : "\"{" + (counter + 1.1) + "," + (counter + 2.1) + "}\"")                               // FLOAT8ARRAY
+                .add(counter == 3 ? NULL : "\"{\\x" + Integer.toHexString(textColumn) + "B," + "\\x" + Integer.toHexString(textColumn + 1) + "B}\"")     // BYTEAARRAY
+                .add(counter == 4 ? NULL : "\"{" + textColumn + textColumn + "," + textColumn + textColumn + "}\"")               // BPCHARARRAY
+                .add(counter == 5 ? NULL : "\"{" + textColumn + textColumn + "," + (textColumn + 1) + "}\"")                      // VARCHARARRAY
+                .add(counter == 0 ? NULL : "\"{2023-01-01,2023-01-02}\"")                                                         // DATEARRAY
+                .add(counter == 1 ? NULL : "\"{93d8f9c0-c314-447b-8690-60c40facb8a5, a56bc0c8-2128-4269-9ce5-cd9c102227b0}\"")     // UUIDARRAY
+                .add(counter == 2 ? NULL : "\"{" + (counter + 1.1) + "," + (counter + 2.1) + "}\"")                                                                      // NUMERICARRAY
+                .add(counter == 3 ? NULL : "\"{12:00:00,13:00:00}\"")                                                             // TIMEARRAY
+                .add(counter == 4 ? NULL : "\"{2023-01-01 12:00:00,2023-01-02 12:00:00}\"")                                       // TIMESTAMPARRAY
+                .add(counter == 5 ? NULL : "\"{2023-01-01 12:00:00+00:00,2023-01-02 12:00:00+00:00}\"")                           // TIMESTAMP_WITH_TIMEZONE_ARRAY
+                .add(counter == 0 ? NULL : "\"{1 hour,2 hours}\"")                                                                // INTERVALARRAY
+                .add(counter == 1 ? NULL : "\"{\"\"{\\\"\"key\\\"\":\\\"\"value\\\"\"}\"\", \"\"{\\\"\"key\\\"\":\\\"\"value\\\"\"}\"\"}\"")   // JSONARRAY
+                .add(counter == 2 ? NULL : "\"{\"\"{\\\"\"a\\\"\":" + counter + "}\"\"}\"")   // JSONBARRAY
+                .add(filter)                                                                            // Filter
                 .toString();
         String key = Integer.toString(counter);
 
