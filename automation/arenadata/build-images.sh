ff6e570b1f3bd0f9126006925ba7575dd46359f2
Sinkov Denis <d.sinkov@arenadata.io>
2024-08-19 18:35:08 +1000

    ADBDEV-6069 Resolve conflicts

--- automation/arenadata/build-images.sh
+++ automation/arenadata/build-images.sh
@@ -7,13 +7,6 @@ pushd ../../server
 ./gradlew clean
 popd
 
-echo "===================================="
-echo "      Build Hadoop 3.3.6 image      "
-echo "===================================="
-pushd hadoop
-docker build -f Dockerfile -t cloud-hub.adsw.io/library/pxf-hadoop:3.3.6 .
-popd
-
 echo "=============================="
 echo "Build PXF image for automation"
 echo "=============================="
