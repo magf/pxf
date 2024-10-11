88ec0194dc6bdab32b83768f8e41a39fbdfee4c0
Viacheslav Vasiliev <v.vasiliev@arenadata.io>
2024-10-11 19:18:13 +0300

    ADBDEV-6431: Drop exTable in afterMethod

--- automation/src/test/java/org/greenplum/pxf/automation/BaseTestParent.java
+++ automation/src/test/java/org/greenplum/pxf/automation/BaseTestParent.java
@@ -246,6 +246,9 @@ public abstract class BaseTestParent {
      * @throws Exception if an error occurs
      */
     protected void afterMethod() throws Exception {
+        if (gpdb != null && exTable != null) {
+            gpdb.dropTable(exTable, true);
+        }
     }
 
     /**
