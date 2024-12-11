90e1b44a10409b1334f6057dcc459f87da23e718
Viacheslav Vasiliev <v.vasiliev@arenadata.io>
2024-12-11 20:04:05 +0300

    ADBDEV-6454: Allure integration

--- automation/src/main/java/listeners/TestAnalyzer.java
+++ automation/src/main/java/listeners/TestAnalyzer.java
@@ -3,6 +3,7 @@ package listeners;
 import annotations.FailsWithFDW;
 import annotations.SkipForFDW;
 import annotations.WorksWithFDW;
+import io.qameta.allure.Allure;
 import org.greenplum.pxf.automation.utils.system.FDWUtils;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
@@ -11,17 +12,32 @@ import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Method invocation listener that skips tests that are not annotated as working with FDW when ran in FDW context.
+ * It also assigns each test to particular suite in Allure.
  */
-public class FDWSkipTestAnalyzer implements IInvokedMethodListener {
+public class TestAnalyzer implements IInvokedMethodListener {
 
     @Override
     public void beforeInvocation(IInvokedMethod invokedMethod, ITestResult result) {
         Method method = result.getMethod().getConstructorOrMethod().getMethod();
         if (method == null) {
             return;
+        }
+        if (method.isAnnotationPresent(Test.class)) {
+            List<String> groups = Arrays.asList(invokedMethod.getTestMethod().getGroups());
+            if (groups.contains("smoke")) {
+                Allure.suite("Smoke");
+            } else if (groups.contains("gpdb")) {
+                Allure.suite("GPDB");
+            } else if (groups.contains("renadata")) {
+                Allure.suite("Arenadata");
+            } else {
+                Allure.suite("Other");
+            }
         }
         // check only @Test annotated method, not @Before.. and @After.. ones
         if (FDWUtils.useFDW && method.isAnnotationPresent(Test.class)) {
