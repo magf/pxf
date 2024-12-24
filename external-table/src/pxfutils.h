c5d24f71e5e44d92d1e19248a7efdc3199a5254a
Andrey Sokolov <a.sokolov@arenadata.io>
2024-12-24 17:18:24 +0300

    Implement TLS options for PXF external tables (#141)
    TLS options implemented for PXF external tables
    
    The requirement for using TLS is implemented for PXF external tables.
    Communication takes place between the segment node and the PXF server and is
    pointless without the PXF server. Testing should be done in conjunction with PXF
    server changes and is out of scope for this commit.
    
    PXF external tables are controlled by environment variables. The following
    options are affected by this commit:
    
        PXF_PROTOCOL: Type string, default http. Defines the protocol for
        communication with the PXF server. Possible values are http and https.
        PXF_SSL_CACERT: Type string, default
        /home/gpadmin/arenadata_configs/cacert.pem. Specifies the absolute path to the
        CA certificate that signed the server certificate. Refers to CURLOPT_CAINFO.
        PXF_SSL_CERT: Type string, default client.pem. Specifies the path to the
        client certificate. Refers to CURLOPT_SSLCERT.
        PXF_SSL_CERT_TYPE: Type string, default pem. Defines the type of the client
        certificate. Refers to CURLOPT_SSLCERTTYPE.
        PXF_SSL_KEY: Type string, default empty. Defines the path to the client's SSL
        private key. Refers to CURLOPT_SSLKEY.
        PXF_SSL_KEYPASSWD: Type string, default empty. Defines the password for the
        client SSL private key. Refers to CURLOPT_KEYPASSWD.
        PXF_SSL_VERIFY_PEER: Type long, default 1. Defines whether the client should
        verify the server certificate. Refers to CURLOPT_SSL_VERIFYPEER.
    
    
    ---------
    
    Co-authored-by: Denis Kovalev <d.kovalev@arenadata.io>

--- external-table/src/pxfutils.h
+++ external-table/src/pxfutils.h
@@ -12,6 +12,9 @@ char	   *TypeOidGetTypename(Oid typid);
 /* Concatenate multiple literal strings using stringinfo */
 char	   *concat(int num_args,...);
 
+/* Get protocol for the PXF server URL */
+const char *get_pxf_protocol(void);
+
 /* Get authority (host:port) for the PXF server URL */
 char	   *get_authority(void);
 
@@ -25,17 +28,38 @@ const char *get_pxf_host(void);
  */
 const int  get_pxf_port(void);
 
+const char *get_pxf_ssl_keypasswd(void);
+const char *get_pxf_ssl_cacert(void);
+const char *get_pxf_ssl_cert(void);
+const char *get_pxf_ssl_key(void);
+const char *get_pxf_ssl_certtype(void);
+long get_pxf_ssl_verifypeer(void);
+
 /* Returns the namespace (schema) name for a given namespace oid */
 char	   *GetNamespaceName(Oid nsp_oid);
 
-#define PXF_PROFILE       "PROFILE"
-#define FRAGMENTER        "FRAGMENTER"
-#define ACCESSOR          "ACCESSOR"
-#define RESOLVER          "RESOLVER"
-#define ANALYZER          "ANALYZER"
-#define ENV_PXF_HOST      "PXF_HOST"
-#define ENV_PXF_PORT      "PXF_PORT"
-#define PXF_DEFAULT_HOST  "localhost"
-#define PXF_DEFAULT_PORT  5888
+#define PXF_PROFILE                 "PROFILE"
+#define FRAGMENTER                  "FRAGMENTER"
+#define ACCESSOR                    "ACCESSOR"
+#define RESOLVER                    "RESOLVER"
+#define ANALYZER                    "ANALYZER"
+#define ENV_PXF_HOST                "PXF_HOST"
+#define ENV_PXF_PORT                "PXF_PORT"
+#define ENV_PXF_PROTOCOL            "PXF_PROTOCOL"
+#define ENV_PXF_SSL_CACERT          "PXF_SSL_CACERT"
+#define ENV_PXF_SSL_CERT            "PXF_SSL_CERT"
+#define ENV_PXF_SSL_CERT_TYPE       "PXF_SSL_CERT_TYPE"
+#define ENV_PXF_SSL_KEY             "PXF_SSL_KEY"
+#define ENV_PXF_SSL_KEYPASSWD       "PXF_SSL_KEYPASSWD"
+#define ENV_PXF_SSL_VERIFY_PEER     "PXF_SSL_VERIFY_PEER"
+#define PXF_DEFAULT_HOST            "localhost"
+#define PXF_DEFAULT_PORT            5888
+#define PXF_DEFAULT_PROTOCOL        "http"
+#define PXF_DEFAULT_SSL_CACERT      "/home/gpadmin/arenadata_configs/cacert.pem"
+#define PXF_DEFAULT_SSL_CERT        "client.pem"
+#define PXF_DEFAULT_SSL_CERT_TYPE   "pem"
+#define PXF_DEFAULT_SSL_KEY         ""
+#define PXF_DEFAULT_SSL_KEYPASSWD   ""
+#define PXF_DEFAULT_SSL_VERIFY_PEER 1
 
 #endif							/* _PXFUTILS_H_ */
