5931752f86d2910481bc6cb8d4bfbe4789dec1ed
Roman Zolotov <rzolotov@arenadata.io>
2025-08-03 18:42:11 +0300

    ADBDEV-7862: Run PXF integration tests with new image (fix test)

--- automation/sqlrepo/arenadata/jdbc-close-session/sql/query01.sql
+++ automation/sqlrepo/arenadata/jdbc-close-session/sql/query01.sql
@@ -1,4 +1,13 @@
 -- @description query01 for PXF test to check that the session is closed correctly
+-- start_matchsubs
+--
+-- m/ERROR:  PXF server error : Failed to read text of query wrong_file_name.*/
+-- s/ERROR:  PXF server error : Failed to read text of query wrong_file_name.*/ERROR:  PXF server error : Failed to read text of query wrong_file_name/
+--
+-- m/HINT:  Check the PXF logs located in the .*/
+-- s/HINT:  Check the PXF logs located in the .*/HINT:  Check the PXF logs located in the pxf log dir./
+--
+-- end_matchsubs
 SELECT * FROM named_query_read_ext_table ORDER BY id;
 
 SELECT * FROM named_query_wrong_read_ext_table ORDER BY id;
