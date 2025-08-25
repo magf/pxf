1a7e52da9a446ca643da7db5087ae40691bd96c8
Roman Zolotov <rzolotov@arenadata.io>
2025-08-25 12:54:50 +0300

    Update image for test environment

--- automation/arenadata/scripts/compile_pxf_without_test.sh
+++ automation/arenadata/scripts/compile_pxf_without_test.sh
@@ -4,22 +4,28 @@ set -eoux pipefail
 
 GPHOME=/usr/local/greenplum-db-devel
 
-# use a login shell for setting environment
+bash --login -c "mkdir -p ${GPHOME}/pxf"
+
+#  use a login shell for setting environment
 bash --login -c "
 	export PXF_HOME=${GPHOME}/pxf
-	make -C '${PWD}/pxf_src/external-table' install
-  make -C '${PWD}/pxf_src/cli' install
-  make -C '${PWD}/pxf_src/server' install-server
+	make -C ${PWD}/pxf_src/external-table install
+  make -C ${PWD}/pxf_src/cli install
+  make -C ${PWD}/pxf_src/server install-server
 "
 
-bash --login -c "chown -R gpadmin:gpadmin ${GPHOME}"
+bash --login -c "chown -R gpadmin:gpadmin ${GPHOME}/"
 bash --login -c "chown -R gpadmin:gpadmin ${PWD}/pxf_src"
 
 # install pxf extension
 su - gpadmin -c "
-    source '/usr/local/greenplum-db-devel/greenplum_path.sh';
-    source '/home/gpadmin/gpdb_src/gpAux/gpdemo/gpdemo-env.sh';
+    source ${GPHOME}/greenplum_path.sh;
+    if [[ -f gpdb_src/gpAux/gpdemo/gpdemo-env.sh ]]; then
+        source gpdb_src/gpAux/gpdemo/gpdemo-env.sh;
+    fi
     cd ${PWD}/pxf_src/fdw &&
     make install &&
     cd ../external-table/ &&
     make install;"
+
+bash --login -c "chown -R gpadmin:gpadmin ${GPHOME}/pxf/"
