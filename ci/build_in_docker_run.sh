3831d13518e5b39e4147d64c80e852f3b1ac05c6
Maxim Gajdaj <maxim.gajdaj@gmail.com>
2025-11-19 15:39:28 +0300

    Update local build script

--- ci/build_in_docker_run.sh
+++ ci/build_in_docker_run.sh
@@ -7,13 +7,20 @@ export GOPATH=/opt/go
 export DEV_HOME=/home/gpadmin
 export GPHOME=/usr/local/greengage-db-devel
 export PXF_HOME=$GPHOME/pxf
+export PXF_SRC=$DEV_HOME/pxf
 export GGDB_IMAGE=ghcr.io/greengagedb/greengage/ggdb6_ubuntu:latest
 export PATH=$GPHOME/bin:$PXF_HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/go/bin:$GOPATH/bin
 export JAVA_TOOL_OPTIONS='-Dfile.encoding=UTF8'
 export DEBIAN_FRONTEND='noninteractive'
 
 docker run --name pxf_build_in_docker --rm -it \
-  --workdir="$PXF_SRC" \
-  --volume ./:"$PXF_SRC" \
-  --env=GOPATH,DEV_HOME,GPHOME,PXF_HOME,PATH,JAVA_TOOL_OPTIONS,DEBIAN_FRONTEND \
-  $GGDB_IMAGE bash -c "ci/build_in_docker.sh"
+  --workdir=$PXF_SRC \
+  --volume ./:$PXF_SRC \
+  --env=GOPATH \
+  --env=DEV_HOME \
+  --env=GPHOME \
+  --env=PXF_HOME \
+  --env=PATH \
+  --env=JAVA_TOOL_OPTIONS \
+  --env=DEBIAN_FRONTEND \
+  $GGDB_IMAGE bash ci/build_in_docker.sh
